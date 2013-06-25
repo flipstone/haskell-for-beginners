13ebdb51e0ca398af1243638b8218d4baac11aeb
David Vollbracht <david.vollbracht@gmail.com>
2013-06-25 07:33:48 -0400

    Problems for Chapter 5 - Maximum Awesome

---
+++ answers/chapter_05/1_maximum_awesome.hs
@@ -0,0 +1,24 @@
+-- Define a list of all Fibonacci numbers
+
+fib :: Num a => [a]
+fib = [ f n | n <- [1..] ]
+  where f 0 = 0
+        f 1 = 1
+        f n = (f (n - 1)) + (f (n - 2))
+
+-- Implement a recursive function to find
+-- the maximum element in a list of
+-- pairs of elements.
+
+maxPairElement :: Ord a => [(a,a)] => a
+maxPairElement [] = error "Maximum of empty list"
+
+maxPairElement [(x,y)]
+  | x >= y = x
+  | otherwise = y
+
+maxPairElement ((x,y):rest) = max bigger (maxPairElement rest)
+  where bigger = max x y
+
+
+
---
+++ problems/chapter_05/1_maximum_awesome.hs
@@ -0,0 +1,12 @@
+-- Define a list of all Fibonacci numbers
+
+fib :: Num a => [a]
+fib = undefined
+
+-- Implement a recursive function to find
+-- the maximum element in a list of
+-- pairs of elements.
+
+maxPairElement :: Ord a => [(a,a)] => a
+maxPairElement = undefined
+

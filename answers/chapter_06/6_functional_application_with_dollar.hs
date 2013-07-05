8b1905a36641bdae4b5d21ae86624add7320d38e
David Vollbracht <david.vollbracht@gmail.com>
2013-07-05 07:35:58 -0400

    Problems for Chapter 6 - Function Application with $

---
+++ answers/chapter_06/6_functional_application_with_dollar.hs
@@ -0,0 +1,16 @@
+-- Use $ to cut down on the parentheses in
+-- the following definitions
+
+first10Squares = take 10 $ map (^2) [1..]
+someMultiplesOfThree = take 5 $ filter (>100) $ map (*3) [1..]
+largestBuzzUnder100 = maximum $ filter (multipleOf 5) $ filter (multipleOf 3) [1..99]
+  where multipleOf n x = x `mod` n == 0
+
+-- Define a function that applies a list of functions
+-- to a list of values, applying the first function to
+-- the first value, the second function to the second,
+-- and so forth.
+
+applyAll :: [(a -> b)] -> [a] -> [b]
+applyAll = zipWith ($)
+

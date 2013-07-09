0910733e5053c78dd65506743631687c3949397d
David Vollbracht <david.vollbracht@gmail.com>
2013-07-09 07:52:33 -0400

    Problems for Chapter 7 - Data.List
    (no really, for real this time)

--- problems/chapter_07/2_data_list.hs
+++ problems/chapter_07/2_data_list.hs
@@ -6,12 +6,7 @@
 --
 -- Write a function to find the longest word in a sentence.
 --
-import Data.List
-import Data.Function (on)
+
 
 sentence =  "Don't use a big word where a diminutive word will suffice"
-longestWord = maximumBy longer . words
-  where longer word1 word2 = compare (length word1) (length word2)
-
-longestWord' = maximumBy (compare `on` length) . words
 

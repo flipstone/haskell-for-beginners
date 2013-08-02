6b3cda9b7a5c9781024a78e63fdbc1fce9ff0f8d
David Vollbracht <david.vollbracht@gmail.com>
2013-08-02 11:04:58 -0400

    Problems for Chapter 9 - Hello World

--- answers/chapter_09/1_hello_world.hs
+++ answers/chapter_09/1_hello_world.hs
@@ -11,7 +11,7 @@ helloWorld :: IO ()
 -- Write, compile and run a hello world program from scratch
 -- in another file.
 --
-
+-- see 1_hello_world/helloworld.hs
 
 -- Define a function that takes a secret string and returns
 -- an action that will ask the user to guess the secret.
@@ -27,8 +27,15 @@ guessTheSecret secret = do
     else putStrLn "Nope!"
 
 
+-- Define an IO action that purports to generate random
+-- numbers, but always produces 9.
+--
+randomIPromise :: IO Int
+randomIPromise = return 9
+
+
 -- Define a funtion that prompts the user with a given string
--- and reads the users response. It should return the users
+-- and reads the user's response. It should return the user's
 -- response, but ensure there is only one space between words
 -- (and none on the beginning or end). Include a type declaration!
 --
@@ -51,6 +58,19 @@ prompt text = do
 -- of Eternal Peril. Use the prompt function you just wrote, and
 -- write any more functions for yourself to use as you see fit.
 --
+bridgeOfDeath = do
+  name <- prompt "What is your name?"
+  questSuccess <- challenge "What is your quest?"
+                            "To seek the Holy Grail"
+
+  when questSuccess $ do
+    finalSuccess <- case name of
+                      "Robin" -> robinsChallenge
+                      "Arthur" -> arthursChallenge
+                      _ -> genericChallenge
+
+    when finalSuccess (putStrLn "Right. Off you go.")
+
 challenge :: String -> String ->  IO Bool
 challenge promptText expectedAnswer = do
   answer <- prompt promptText
@@ -69,17 +89,25 @@ arthursChallenge = challenge "What is the air-speed velocity of an unladen swall
 genericChallenge = challenge "What is your favorite color?"
                              "Blue"
 
-bridgeOfDeath = do
-  name <- prompt "What is your name?"
-  questSuccess <- challenge "What is your quest?"
-                            "To seek the Holy Grail"
 
-  when questSuccess $ do
-    finalSuccess <- case name of
-                      "Robin" -> robinsChallenge
-                      "Arthur" -> arthursChallenge
-                      _ -> genericChallenge
+-- Define an action which will prompt the user three times
+-- for a number. Then print out the sum, product, maximum, and
+-- minimum of the numbers the user entered. Use some of the IO
+-- helpers discussed in the text to make your life easier.
+--
+numStats :: IO ()
+numStats = do
+  numberTexts <- sequence (replicate 3 $ prompt "Enter a number")
+  let numbers = map read numberTexts
+  mapM_ (showStat numbers) [("Sum", sum),
+                            ("Product", product),
+                            ("Maximum", maximum),
+                            ("Minimum", minimum)]
 
-    when finalSuccess (putStrLn "Right. Off you go.")
+showStat :: [Int] -> (String, [Int] -> Int) -> IO ()
+showStat nums (name, stat) = do
+  putStr name
+  putStr ": "
+  print (stat nums)
 
 
---
+++ problems/chapter_09/1_hello_world.hs
@@ -0,0 +1,54 @@
+-- Define an action below that will print a hello world message.
+-- Execute the program in GHCI.
+
+import Control.Monad (when)
+
+helloWorld = undefined
+
+-- Add a type declaration for helloWorld.
+
+-- Write, compile and run a hello world program from scratch
+-- in another file.
+--
+-- see 1_hello_world/helloworld.hs
+
+-- Define a function that takes a secret string and returns
+-- an action that will ask the user to guess the secret.
+-- Tell the user whether they guessed the secret correctly.
+-- Be sure to include a type declaration.
+--
+
+
+-- Define an IO action that purports to generate random
+-- numbers, but always produces 9.
+--
+
+
+-- Define a funtion that prompts the user with a given string
+-- and reads the user's response. It should return the user's
+-- response, but ensure there is only one space between words
+-- (and none on the beginning or end). Include a type declaration!
+--
+
+
+-- Define an action that will ask the user 3 questions before they
+-- may pass over the bridge. The first two questions always ask for
+-- their name and quest. Normally the last question asks their
+-- favorite color, but knights named Robin are asked what the
+-- capital of Assyria is, and kings name Arthur are queried about the
+-- speed of unbaggaged airborn fowl.
+--
+-- Judge the answers however you see fit, and tell the user whether
+-- they are allowed to cross the bridge or are cast into the Gorge
+-- of Eternal Peril. Use the prompt function you just wrote, and
+-- write any more functions for yourself to use as you see fit.
+--
+
+
+-- Define an action which will prompt the user three times
+-- for a number. Then print out the sum, product, maximum, and
+-- minimum of the numbers the user entered. Use some of the IO
+-- helpers discussed in the text to make your life easier.
+--
+
+

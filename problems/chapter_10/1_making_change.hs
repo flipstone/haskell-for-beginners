329f5a3dc26cd1bd932e3fc2a69a6fc5f688576d
David Vollbracht <david.vollbracht@gmail.com>
2013-08-25 07:15:38 -0400

    Fix typo: demonination -> denomination

--- problems/chapter_10/1_making_change.hs
+++ problems/chapter_10/1_making_change.hs
@@ -2,14 +2,14 @@
 -- program should accept a list of numbers
 -- on stdin. The first number indicates the
 -- amount of money to change, the rest
--- indicate the demoninations of coin to
+-- indicate the denominations of coin to
 -- make change into.
 --
 -- The program should print out a list of
--- coins in the demoninations given that totals
+-- coins in the denominations given that totals
 -- up to the amount of money being changed.
 -- If there is money left over (i.e. the money
--- cannot be divided evenly into the demoninations
+-- cannot be divided evenly into the denominations
 -- given), the program should print the list of
 -- coins and a message indicating how much
 -- was left unchanged.

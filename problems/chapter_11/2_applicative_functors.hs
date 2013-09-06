b6ede5d04ac9c7efa71bce1b970072b742bd2df3
David Vollbracht <david.vollbracht@gmail.com>
2013-09-06 07:31:10 -0400

    Rest of problems for Chapter 11 - Applicative Functors

--- problems/chapter_11/2_applicative_functors.hs
+++ problems/chapter_11/2_applicative_functors.hs
@@ -7,7 +7,7 @@ import Data.Char
 -- <$> either!)
 --
 getIntLine :: IO Int
-getIntLine = pure read <*> getLine
+getIntLine = undefined
 
 
 -- Define addition for Applicatives using
@@ -16,32 +16,21 @@ getIntLine = pure read <*> getLine
 -- in GHCI to see how it behaves. Try Maybe, lists,
 -- IO, and functions.
 --
-aPlus :: (Num a, Applicative f) => f a -> f a -> f a
-aPlus x y = (+) <$> x <*> y
+aPlus = undefined
 
 -- Use lists an Applicatives to calculate the chances
 -- of rolling 15 or better on a roll of 1d6 + 1d10 + 1d4.
 --
-p = num15OrOver / numPossibilities
-  where allPossibilities = [1..6] `aPlus` [1..10] `aPlus` [1..4]
-        _15OrOver = filter (>=15) allPossibilities
-        num15OrOver = fromIntegral (length _15OrOver)
-        numPossibilities = fromIntegral (length allPossibilities)
+p = undefined
 
 -- Reimplement the encrypt and decrypt functions from
 -- Chapter 7, problem set 2 using ZipLists as Applicatives.
 --
 encrypt :: String -> String -> String
-encrypt pad plainText = getZipList $ padOne <$>
-                        ZipList (cycle pad) <*>
-                        ZipList plainText
-  where padOne p c = chr (ord c + digitToInt p)
+encrypt = undefined
 
 decrypt :: String -> String -> String
-decrypt pad plainText = getZipList $ padOne <$>
-                        ZipList (cycle pad) <*>
-                        ZipList plainText
-  where padOne p c = chr (ord c - digitToInt p)
+decrypt = undefined
 
 cipherText = "Xiss-us-pr\128tr\128-\131i\129v-fi}Zv\DELt\128/"
 key = "c0ded"
@@ -50,26 +39,23 @@ key = "c0ded"
 -- Pick an Applicative and demonstrate that all the Applicative laws
 -- are satisfied for it.
 --
-fmapProof = (pure (+1) <*> [3,4]) == fmap (+1) [3,4]
-idProof = (pure id <*> [3,4]) == [3,4]
-compositionProof = (pure (.) <*> [(+1),(+2)] <*> [(*2),(*3)] <*> [4,5]) ==
-                   ([(+1),(+2)] <*> ([(*2),(*3)] <*> [4,5]))
-purityProof = (pure (+1) <*> pure 3) == (pure (3 + 1) :: [Int])
-dollarProof = ([(+1),(+3)] <*> pure 3) == (pure ($3) <*> [(+1),(+3)])
+fmapProof = undefined
+idProof = undefined
+compositionProof = undefined
+purityProof = undefined
+dollarProof = undefined
 
 
 -- Redefine addition on Applicatives using lifting
 --
-aPlus' :: (Num a, Applicative f) => f a -> f a -> f a
-aPlus' = liftA2 (+)
+aPlus' = undefined
 
 
 -- Define a function to find the sum of a list
 -- of Ints that are wrapped in an Applicative.
 -- Try it out on some Applicatives in GHCI.
 --
-sumA :: Applicative f => [f Int] -> f Int
-sumA = foldl aPlus' (pure 0)
+sumA = undefined
 
 
 -- Define a left folding function for Applicatives
@@ -77,11 +63,10 @@ sumA = foldl aPlus' (pure 0)
 -- initial value, but operates on a list of Applicatives
 --
 foldlA :: Applicative f => (a -> b -> a) -> a -> [f b] -> f a
-foldlA f = foldl (liftA2 f) . pure
+foldlA = undefined
 
 -- Redefine sum for Applicatives using your new fold
 -- function for Applicatives.
 --
-sumA' :: Applicative f => [f Int] -> f Int
-sumA' = foldlA (+) 0
+sumA' = undefined
 
